e3459d2ef42a6012865cd01db445a95f404f56b4
Lightning3240 <Lightning3240@outlook.com>
2019-06-13 20:19:23 +0100

    Add files via upload

---
+++ Downloadable app/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CefSharp;
+using CefSharp.WinForms;
+using CefSharp.WinForms.Internals;
+
+
+namespace Squabble
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public CefSharp.WinForms.ChromiumWebBrowser browser;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            browser = new CefSharp.WinForms.ChromiumWebBrowser("https://squabble.glitch.me/app.html")
+            {
+                Dock = DockStyle.Fill,
+                Size = new Size(600, 600),
+                Location = new Point(200, 200),
+            };
+            this.panel1.Controls.Add(browser);
+        }
+    }
+}
